f9323d091cbd2bcb10e49cb20ffe81ea316a4bd8
Hiroto-Terai <hiro08051610@gmail.com>
2021-12-15 02:44:12 +0900

    プロジェクト作成

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour
+{
+  public Text CountText;
+  public int Cnt = 0;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void OnClick(int number)
+  {
+    switch (number)
+    {
+      case 0:
+        {
+          Cnt--;
+          CountText.text = "カウント: " + Cnt;
+          break;
+        }
+      case 1:
+        {
+          Cnt++;
+          CountText.text = "カウント: " + Cnt;
+          break;
+        }
+      default:
+        break;
+    }
+  }
+}
